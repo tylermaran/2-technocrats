5b9197356ff4b4b2b55b65f4d81581c3ae758c66
lstroly <lstroly@gmail.com>
2018-04-19 17:11:33 -0400

    front-end skeleton with react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import "./App.css";
+
+
+class App extends Component {
+
+  // Map over this.state.pics and render a PicCard component for each pic object
+  render() {
+    return (
+    <Router>
+      <div className="container-fluid">
+        <Navbar />
+        <Wrapper>
+          <Title />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,73 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Navbar.css";
+console.log("testing!!!");
+// Depending on the current path, this component sets the "active" classNameName on the appropriate navigation link item
+const Navbar = props => (
+  <div className="col-4 col-lg-3 bd-sidebar" id="bd-docs-nav">
+    <nav>
+      <div className="bd-toc-item" id="brandName">
+        Tech Square Investments
+      </div>
+      <div className="bd-toc-item" id="studentName">
+        John J. Schmidt
+      </div>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link" id="stocks">Stocks</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-MSFT</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-SPY</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-FB</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-APPL</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-TSLA</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-AMZN</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-USO</li>
+      </ul>
+      <ul className="nav bd-sidenav">
+        <li classNamme="bd-toc-link">-DOW</li>
+      </ul>
+    </nav>
+  </div>
+
+  // <nav classNameNameName="navbar navbar-default">
+  //   <div classNameNameName="container-fluid">
+  //     <div classNameNameName="navbar-header">
+  //       <Link classNameNameName="navbar-brand" to="/">
+  //         Tech Square Investments
+  //       </Link>
+  //     </div>
+  //     <ul classNameNameName="nav navbar-nav">
+  //       <li
+  //         classNameNameName={
+  //           window.location.pathname === "/" || window.location.pathname === "/about"
+  //             ? "active"
+  //             : ""
+  //         }
+  //       >
+  //         <Link to="/">About</Link>
+  //       </li>
+  //       <li classNameNameName={window.location.pathname === "/discover" ? "active" : ""}>
+  //         <Link to="/discover">Discover</Link>
+  //       </li>
+  //       <li classNameNameName={window.location.pathname === "/search" ? "active" : ""}>
+  //         <Link to="/search">Search</Link>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // </nav>
+);
+
+export default Navbar;
