ab65abb1c1d3f5b6cfedf8bf502bc5980dd35086
priscila <deavila.priscila@outlook.com>
2018-04-21 15:19:59 -0700

    Point

---
+++ client/src/pages/LoginPage/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { field, reduxForm } from 'redux-form';
+// import renderField from 'component/ FormInputs/renderField';
+
+// const validate = values => {
+//     const errors = {} ;
+//     if (values.email !== undefined) {
+//         errors.email = 'Email is required';
+//     }
