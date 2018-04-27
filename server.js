39f06e0e1626ff2c84a4144335c0aabe9e21f5a6
Matt Gohr <gohr89@gmail.com>
2018-04-26 21:37:09 -0700

    api's working

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const routes = require("./routes");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,15 +15,15 @@ app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 //Use Routes
-app.use('./api/users', users);
-app.use("./api/profile", profile);
-app.use("./api/posts", posts);
+app.use('/api/users', users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 
 // Start the API server
