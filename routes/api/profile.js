0da014b19fcb11c503c2853f976a089841ec4b8d
Matt Gohr <gohr89@gmail.com>
2018-05-05 01:56:55 -0400

    profile route get request with jwt working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const passport = require("passport");
 
 // Load user porfile
-const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const Profile = require("../../models/Profile");
+const User = require("../../models/Users");
 
 // @route   GET api/us/test
 // @desc    Test post route
@@ -15,10 +15,22 @@ router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 // @route   GET api/profile
 // @desc    get current users profile
 // @access  Private
-router.get("/", passport.authenticate('jwt', {session: false}), (req, res) => {
-
-})
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "No profile exists for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 // res.json({ msg: "Profile Works" }));
-
 
 module.exports = router;
