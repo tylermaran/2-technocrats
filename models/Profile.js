5a5fff5152f21e5f109f9184186854e05b26f55f
Matt Gohr <gohr89@gmail.com>
2018-05-05 01:37:35 -0400

    patch profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //create Schema
-const UserSchema = new Schema({
+const ProfileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'users'
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load user porfile
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
 // @route   GET api/us/test
 // @desc    Test post route
-// @access  Private
+// @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route   GET api/profile
+// @desc    get current users profile
+// @access  Private
+router.get("/", passport.authenticate('jwt', {session: false}), (req, res) => {
+
+})
+// res.json({ msg: "Profile Works" }));
+
 
 module.exports = router;
