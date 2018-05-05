ce7e4de83c08470bdf306fa4ffeb2d7311ffd081
Matt Gohr <gohr89@gmail.com>
2018-05-05 13:54:03 -0400

    Moving all components to ProfilePage

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -16,73 +16,130 @@ let min = 0;
 let max = 1000;
 
 class ProfilePage extends Component {
-  state = {
+    state = {
     stocks,
     priceArray: [],
-    title: ""
+    title: "",
+    timeline: [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+    "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+    dayLimit: 30
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    // this.callApi()
+    //   .then(res => this.setState({ response: res.express }))
+    //   .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/users/test");
+    const body = await response.json();
+    console.log(body)
+    if (response.status !== 200) throw Error(`my error ${body.message}`);
+
+    return body;
+  };
+  // state = {
+  //   stocks
+  // };
 
   handleClick = id => {
-    const stock = this.state.stocks.find(item => item.id === id);
+
+    const stock = this.state.stocks.find( item => item.id === id );
     title = stock.title;
     const ticker = stock.title.toUpperCase(); // TODO: maybe unneeded??
 
     //  Pull stock data based on parameters
     var parameters = {
       symbols: ticker,
-      types: "chart,news",
-      range: "1y",
-      last: "5"
-    };
+      types: 'chart,news',
+      range: '1y',
+      last: '5'
+    }
     axios({
-      method: "GET",
-      url: "https://api.iextrading.com/1.0//stock/market/batch",
+      method: 'GET',
+      url: 'https://api.iextrading.com/1.0//stock/market/batch',
       params: parameters
-    }).then(response => {
-      var stockData = response.data[ticker];
-      console.log(stockData);
-      const dayLimit = 30;
+    })
+      .then((response) => {
 
-      const priceArray = [];
-      for (let i = 0; i < dayLimit; i++) {
-        priceArray.push(stockData.chart[i].close);
-      }
+        var stockData = response.data[ticker];
+        console.log(stockData);
 
-      news = stockData.news[0].headline;
 
-      min = Math.min.apply(Math, priceArray);
-      max = Math.max.apply(Math, priceArray);
+        const priceArray = [];
+        for ( let i = 0; i < this.state.dayLimit; i++ ) {
+          priceArray.push( stockData.chart[i].close );
+        }
 
-      // const priceArray = stockData.chart
-      //   .map( quote => quote.close )
-      //   .filter( (quote, i) => i < dayLimit );
+        news = stockData.news[0].headline;
 
-      this.setState({ priceArray });
-    });
-  };
+        min = Math.min.apply(Math, priceArray);
+        max = Math.max.apply(Math, priceArray);
+
+        // const priceArray = stockData.chart
+        //   .map( quote => quote.close )
+        //   .filter( (quote, i) => i < dayLimit );
+
+        this.setState({ priceArray })
+
+
+
+      });
+
+    // testing get/post routing
+    axios({
+      method: 'GET',
+      url: '/api/users/test',
+    })
+      .then((response) => {
+        console.log(response);
+        console.log("it works");
+      });
+
+  }
+
+  displayWeek = () => {
+    this.setState({  timeline: [ "1", "2", "3", "4", "5", "6", "7"],
+    dayLimit:7 })
+  }
+
+  displayMonth = () => {
+    this.setState({timeline: [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+    "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+  dayLimit: 30 })
+  }
+
+
+
 
   render() {
     return (
-        <div className="container-fluid">
-          <Navbar handleClick={this.handleClick} />
-          <Wrapper>
-            <Title />
-            <Graph
-              title={title}
-              priceArray={this.state.priceArray}
-              min={min}
-              max={max}
+      <div className="app">
+        <Navbar handleClick={this.handleClick}/>
+        <Wrapper>
+          <Title />
+          <Graph
+            title={title}
+            priceArray={this.state.priceArray}
+            min={min}
+            max={max}
+            timeline={this.state.timeline}
+            displayWeek={this.displayWeek}
+            displayMonth={this.displayMonth}
+          />
+          <div className="row">
+            <News
+              news={news}
             />
-            <div className="row">
-              <News news={news} />
-              <PieChart />
-            </div>
-          </Wrapper>
-        </div>
-    );
-  }
+            <PieChart />
+          </div>
+        </Wrapper>
+      </div>
+  );
+}
 }
 
 export default ProfilePage;
