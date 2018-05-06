84b0dc407e2d67430a06fac977404a8345bfcf05
Matt Gohr <gohr89@gmail.com>
2018-05-06 16:06:46 -0400

    register and login working with state

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,40 +1,75 @@
 import React, { Component } from "react";
 import NavBarTop from "../components/Navbar/NavBarTop.js";
 
-class Login extends Component {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    const loginUser = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    console.log(loginUser);
+  }
+
   render() {
     return (
       <div>
         <NavBarTop />
-        <form>
-          <div className="form-group row">
-            <label for="example-email-input" className="col-2 col-form-label">
-              Email
-            </label>
-            <div className="col-10">
-              <input className="form-control" type="email" id="input" />
+        <div className="container">
+          <form onSubmit={this.onSubmit}>
+         
+            <div className="form-group row">
+              <p className="col-2 col-form-label">Email</p>
+              <div className="col-5">
+                <input
+                  className="form-control"
+                  type="email"
+                  id="email"
+                  placeholder="example@example.com"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+              </div>
             </div>
-          </div>
-          <div className="form-group row">
-            <label
-              for="example-password-input"
-              className="col-2 col-form-label"
-            >
-              Password
-            </label>
-            <div className="col-10">
-              <input
-                className="form-control"
-                type="password"
-                value="hunter2"
-                id="example-password-input"
-              />
+            <div className="form-group row">
+              <p className="col-2 col-form-label">Password</p>
+              <div className="col-5">
+                <input
+                  className="form-control"
+                  type="password"
+                  placeholder="12345"
+                  name="password"
+                  id="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+              </div>
             </div>
-          </div>
-        </form>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default Login;
+export default Register;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -8,15 +8,29 @@ class Register extends Component {
       name: "",
       email: "",
       password: "",
-      passwords: "",
+      password2: "",
       errors: {}
     };
 
-    this.onChange = this.onChange.bing(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange(e){
-    this.setState({[e.target.name]: e.target.value})
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+
+    console.log(newUser);
   }
 
   render() {
@@ -24,75 +38,65 @@ class Register extends Component {
       <div>
         <NavBarTop />
         <div className="container">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group row">
-              <label for="example-email-input" className="col-2 col-form-label">
-                Name
-              </label>
-              <div className="col-10">
-                <input 
-                className="form-control" 
-                type="name" 
-                id="input" 
-                placeholder="First and Last Name"
-                name="name"
-                value={this.state.name}
-                onChange={this.onChange}
+              <p className="col-2 col-form-p">Name</p>
+              <div className="col-5">
+                <input
+                  className="form-control"
+                  type="name"
+                  id="name"
+                  placeholder="First and Last Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
                 />
               </div>
             </div>
             <div className="form-group row">
-              <label for="example-email-input" className="col-2 col-form-label">
-                Email
-              </label>
-              <div className="col-10">
-                <input 
-                className="form-control" 
-                type="email" 
-                id="input" 
-                placeholder="example@example.com"
-                name="email"
-                value={this.state.email}
-                onChange={this.onChange}
+              <p className="col-2 col-form-label">Email</p>
+              <div className="col-5">
+                <input
+                  className="form-control"
+                  type="email"
+                  id="email"
+                  placeholder="example@example.com"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
                 />
               </div>
             </div>
             <div className="form-group row">
-              <label
-                for="example-password-input"
-                className="col-2 col-form-label"
-              >
-                Password
-              </label>
-              <div className="col-10">
+              <p className="col-2 col-form-label">Password</p>
+              <div className="col-5">
                 <input
                   className="form-control"
                   type="password"
                   placeholder="12345"
-                  id="password2"
+                  name="password"
+                  id="password"
                   value={this.state.password}
                   onChange={this.onChange}
                 />
               </div>
             </div>
             <div className="form-group row">
-              <label
-                for="example-password-input"
-                className="col-2 col-form-label"
-              >
-                Re Enter Password
-              </label>
-              <div className="col-10">
+              <p className="col-2 col-form-label">Re Enter Password</p>
+              <div className="col-5">
                 <input
                   className="form-control"
                   type="password"
                   placeholder="12345"
+                  name="password2"
                   id="password2"
                   value={this.state.password2}
                   onChange={this.onChange}
                 />
               </div>
             </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
           </form>
         </div>
       </div>
