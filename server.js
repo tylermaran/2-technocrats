828a81d2b6443d3e2cbb7fa1a654979ca065224b
Matt Gohr <gohr89@gmail.com>
2018-05-07 22:43:06 -0400

    change server again for heroku

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static("client/build"));
   console.log("were in PRODUCTION BABY!")
 
-  // app.get("*", (req, res) => {
-  //   res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Connect to the Mongo DB
