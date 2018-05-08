9e5944a2001b810d9b402702d80839907b896a79
lstroly <lstroly@gmail.com>
2018-05-08 19:07:48 -0400

    convert navbar to class

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -14,58 +14,28 @@ class Navbar extends React.Component {
 
   render() {
     return (
-  <div className="col-4 col-lg-3 bd-sidebar" id="bd-docs-nav">
-    <nav>
-      <div className="bd-toc-item" id="brandName">
-        Tech Square Investments
+      <div className="col-4 col-lg-3 bd-sidebar" id="bd-docs-nav">
+        <nav>
+          <div className="bd-toc-item" id="brandName">
+            Tech Square Investments
+          </div>
+          <div className="bd-toc-item" id="studentName">
+            John J. Schmidt
+          </div>
+          <div className="bd-toc-item" id="studentName">
+            Class #123
+          </div>
+        </nav>
+        {stocks.map(stock => (
+          <NavLinks
+            handleClick={this.props.handleClick}
+            name={stock.name}
+            id={stock.id}
+            />
+
+        ))}
       </div>
-      <div className="bd-toc-item" id="studentName">
-        John J. Schmidt
-      </div>
-      <div className="bd-toc-item" id="studentName">
-        Class #123
-      </div>
-    </nav>
-   {stocks.map(stock => (
-      <NavLinks
-        handleClick={this.props.handleClick}
-        name={stock.name}
-        id={stock.id}
-     />
+    )}
+  };
 
-    ))}
-    </div>
-
-
-)}
-
-
-  // <nav classNameNameName="navbar navbar-default">
-  //   <div classNameNameName="container-fluid">
-  //     <div classNameNameName="navbar-header">
-  //       <Link classNameNameName="navbar-brand" to="/">
-  //         Tech Square Investments
-  //       </Link>
-  //     </div>
-  //     <ul classNameNameName="nav navbar-nav">
-  //       <li
-  //         classNameNameName={
-  //           window.location.pathname === "/" || window.location.pathname === "/about"
-  //             ? "active"
-  //             : ""
-  //         }
-  //       >
-  //         <Link to="/">About</Link>
-  //       </li>
-  //       <li classNameNameName={window.location.pathname === "/discover" ? "active" : ""}>
-  //         <Link to="/discover">Discover</Link>
-  //       </li>
-  //       <li classNameNameName={window.location.pathname === "/search" ? "active" : ""}>
-  //         <Link to="/search">Search</Link>
-  //       </li>
-  //     </ul>
-  //   </div>
-  // </nav>
-};
-
-export default Navbar;
+  export default Navbar;
